7cbd96d7e42fd31932bc9bc76fa597277fd84647
Md Al Mamun <mamunju2015@gmail.com>
2025-01-14 07:02:50 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,188 @@
+// let num1=10;
+// let num2=20;
+// let num3=20;
+
+// if (num1 < num3){
+//     console.log (num3 + "is the largest number")
+// }
+
+
+// if (num1>num2){
+
+//     console.log(num1+"is the largest number");
+// }else {  console.log(num3 +"is the largest number");}
+
+
+// if (num1>num2){
+
+//     console.log(num1+"is the largest number");
+// }else if (num2 > num3) {  console.log(num2 +"is the largest number");}else {  console.log(num3 +"is the largest number");}
+
+
+
+
+// let age=18;
+// let nid=true;
+
+// if (age>=18){
+//     if(nid){console.log("you are eligable");} else {console.log("please bring nid")}
+
+
+// }else{
+
+// console.log("you are not eligable")
+// }
+
+
+// let day = parseInt(prompt("Enter a day number (1-7):"));
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+
+// for( let i=1; i<=5; i++){
+
+//     console.log(i);
+    
+//     }
+
+
+
+// for( let i=1; i<=10; i+=2){
+
+//     console.log(i);
+    
+//     }
+    
+
+
+// for( let i=1; i<=10; i+=2){
+//     if(i==5||i==7){continue;}
+
+//     console.log(i);
+    
+//     }
+
+
+
+
+// for( let i=1; i<=10; i+=2){
+//     if(i==5){break;}
+
+//     console.log(i);
+    
+//     }
+
+
+
+// let factNumber=5;
+// let result=1;
+
+// for( let i=1; i<=factNumber; i++){
+
+//     result *=i;
+// }
+
+// console.log(result);
+
+
+
+
+
+// let start=1;
+// let end=20;
+
+// for (start;start <=end; start++){
+// if (start%2==0){console.log(start);
+
+// }
+
+// }
+
+
+
+// let start=1;
+// let end=30;
+
+// let oddCount=0;
+// let evenCount=0;
+
+// for(start;start<=end; start++){
+// if (start%2==0){evenCount++;}else{
+//     oddCount++;}
+// }
+
+// console.log(evenCount);
+// console.log(oddCount);
+
+
+
+// let start=1;
+// let end=10;
+
+// do{
+ 
+//     console.log(start);
+//     start++;
+
+// }while(start<=end);
+
+
+
+
+
+
+/*****************febonakki seris class 27 **** */
+
+
+// let limit=10;
+
+// let num1=0;
+// let num2=1;
+
+// console.log(num1);
+// console.log(num2);
+
+// for (let i=1; i<=limit; i++){
+// let nextValue=num1+num2;
+// console.log(nextValue);
+// num1=num2;
+// num2=nextValue;
+
+// }
+
+
+
+/******************************************* */
+
+
+/*  make a * peramid with loop  */
+/* syncronus and accyncronus */
+/* High order function
+/*constructor function
+
+/******************************************* */
